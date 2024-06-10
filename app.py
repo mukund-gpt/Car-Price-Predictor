cdb25269bd5cfbb05989d5892a3b9e67e78a049a
Mukund <abhishek.jupyter@gmail.com>
2024-06-10 10:00:57 +0530

    flask

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template
+import pandas as pd
+app=Flask(__name__)
+car=pd.read_csv('Cleaned car.csv')
+
+
+
+@app.route('/')
+def index():
+    companies=sorted(car['company'].unique())
+    car_models=sorted(car['name'].unique())
+    year=sorted(car['year'].unique(),reverse=True)
+    fuel_type=sorted(car['fuel_type'].unique())
+    return render_template('index.html',companies=companies,car_models=car_models,years=year,fuel_types=fuel_type)
+
+
+
+if __name__ =="__main__":
+    app.run(debug=True)
